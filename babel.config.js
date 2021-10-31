f35b26b83559f5b55b9a24f084124ce501fb19f7
albrimpaqarizi <albrim.paqarizi@ornio.no>
2021-10-31 18:42:33 +0100

    dev:  added config for absolute paths for imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          cwd: 'babelrc',
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          root: ['./'],
+        },
+      ],
+      'jest-hoist',
+    ],
   };
 };
